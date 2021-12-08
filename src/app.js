f040e7b3cbaa684a35ec1b24b54d052773ee1036
Yetsibeth Nieto <yetsicaron@gmail.com>
2021-12-08 06:34:41 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var quien = [
+  "Un perro ",
+  "Una vaca ",
+  "Un extraterrestre ",
+  "Un chancho ",
+  "Mi abuela ",
+  "Tu mama "
+];
+var que = [" se comio ", " se meo sobre ", " se robo ", "salio corriendo con "];
+var miQue = [" mi tarea ", " mi sandwich ", " mi comida ", " mi cama "];
+var donde = [" el supermercado", " la plaza ", " la playa ", " la montaña"];
+
+function ran(arr) {
+  var r = Math.random() * arr.length;
+  console.log(r);
+  return Math.floor(r);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var ex = document.getElementById("excusa");
+  ex.innerHTML =
+    quien[ran(quien)] +
+    que[ran(que)] +
+    miQue[ran(miQue)] +
+    "en" +
+    donde[ran(donde)];
 };
